7c602c79a56c40a08665952eddf5a89a6b6a9566
Dimas Wihandono <daimessdn@gmail.com>
2021-02-23 20:37:49 +0700

    testing for the tone.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
+import * as Tone from "tone";
+
 import './App.css';
 
 function App() {
+  const synth = new Tone.Synth().toDestination();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello, world!</h1>
+        <button onClick={() => {synth.triggerAttackRelease("C4", "8n")}}>Click me</button>
       </header>
     </div>
   );
